18e401f9eeeaf46fb61bded598822f2815c249aa
TranThienPhucB2014775 <phuctran2002tpt@gmail.com>
2023-09-19 16:16:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const contactRouter = require("./app/routes/contact.route")
+const ApiError = require("./app/api-error")
 
 const app = express();
 
@@ -10,5 +11,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactRouter);
+
+//handle 404 response
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error || "Internal Server Error",
+    });
+});
 
 module.exports = app;
